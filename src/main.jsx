f1e933ab28d618239391e9179f69d3efa5bdbc4c
iversson-estudos <i versson.estudos@gmail.com>
2025-04-18 23:47:05 -0300

    working on the css modules, seems complicated

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import Homepage from './pages/homepage/Homepage'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Homepage/>
   </StrictMode>,
 )
---
+++ src/pages/homepage/Homepage.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+import styles from './Homepage.module.css';
+
+
+export default function Homepage(){
+return(
+<>
+<p className={styles.para}>Welcome to the CV Application</p>
+<button className={styles.btn}>Lets Go</button>
+</>
+)}
