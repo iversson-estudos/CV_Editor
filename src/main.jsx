acf9d5c4e4a2d5efc688a9e2f3c46dfb3f70ee7b
iversson-estudos <i versson.estudos@gmail.com>
2025-04-18 23:53:48 -0300

    hihi, I got it done, global css and each component with its own css hihi <3

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,9 @@
+/*CSS IMPORTS*/
+import './assets/css/reset.css';   // Reset first
+import './assets/css/global.css'; // Then globals
+
+/*OTHER IMPORTS*/ 
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Homepage from './pages/homepage/Homepage'
