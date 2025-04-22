459a05d8d03870ae38aafabd954182671969b650
iversson-estudos <i versson.estudos@gmail.com>
2025-04-22 09:58:57 -0300

    minor changes

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -6,7 +6,7 @@ import styles from './Homepage.module.css';
 export default function Homepage(){
 return(
 <>
-<p className={styles.para}>Welcome to the CV Application</p>
+<p className={styles.para}>Welcome to the CV App</p>
 <button className={styles.btn}><span class="text">Lets Go</span></button>
 </>
 )}
