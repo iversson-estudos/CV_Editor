e487f043bad83c84d7ef5a73415b2fef7ac70f84
iversson-estudos <i versson.estudos@gmail.com>
2025-04-25 14:19:00 -0300

    Oh man, need to apply now a lift up the state dinamic

--- src/pages/app/App.jsx
+++ src/pages/app/App.jsx
@@ -6,13 +6,15 @@ export default function App () {
        return (
        <div className={styles.appContainer}>
           <div className={styles.inputsContainer}>
-          <form>
-            <TextInput  divClass={styles.name} name='name' label='Name' placeHolder='Enter your name' type='text' size={24} />
-            <div className={styles.experienceContainer}>
-                <TextLabeled divClass={styles.experience} name='experience' placeHolder='Past Experiences' label='Experience' size={80}/>
-                <TextLabeled divClass={styles.experience} name='experience' placeHolder='Company' label='Company' size={24} />
-            </div>
-          </form>
+              <form>
+                <div className={styles.personalContainer}>
+                  <TextInput  divClass={styles.name} name='name' label='Name' placeHolder='Enter your name' type='text' size={24} />
+                </div>
+                <div className={styles.experienceContainer}>
+                    <TextLabeled divClass={styles.experience} name='experience' placeHolder='Past Experiences' label='Experience' size={80}/>
+                    <TextLabeled divClass={styles.experience} name='experience' placeHolder='Company' label='Company' size={24} />
+                </div>
+              </form>
        </div>
       </div> 
 )}
