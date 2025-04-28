07bca284b1f0726b53c7266f89380b4ad0b63107
iversson-estudos <i versson.estudos@gmail.com>
2025-04-28 15:48:44 -0300

    I want to do something but dont know how

--- src/components/Inputs/TextInputLabeled.jsx
+++ src/components/Inputs/TextInputLabeled.jsx
@@ -10,11 +10,13 @@ export function TextInputLabeled({name,type,size,label,handleChange,divClass,pla
   const inputStyle = {
     height:`${size}px` 
   };
-  
+
+ 
   return (
     <div className={divClass} style={divStyle}> 
       <label htmlFor={name}>{label}</label>
       <input name={name} type={type} id={name} onChange={handleChange} placeholder={placeHolder} style={inputStyle}/>
+      
     </div>
 )}
   
