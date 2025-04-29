61896e887b20eeb0527b6d0ad4982f1b4b19f813
iversson-estudos <i versson.estudos@gmail.com>
2025-04-29 09:49:36 -0300

    added button so the divs are dinamically incremented on user press

--- src/components/Inputs/TextInputLabeled.jsx
+++ src/components/Inputs/TextInputLabeled.jsx
@@ -1,7 +1,7 @@
-import React from "react";
+import {React,useState} from "react";
 
 export function TextInputLabeled({name,type,size,label,handleChange,divClass,placeHolder='...'}) {
-    
+ /*STYLES*/   
   const divStyle = {
     display:'flex', 
     flexDirection:'column'
@@ -10,13 +10,40 @@ export function TextInputLabeled({name,type,size,label,handleChange,divClass,pla
   const inputStyle = {
     height:`${size}px` 
   };
+  
+  /*DINAMIC SIZE*/
+  const [numberOfDivs,setNumberOfDivs] = useState(1);
 
- 
+  const incrementDivs = ()=>{
+    setNumberOfDivs(numberOfDivs+1);
+  }
+
+  function CompleteComponent(){
+    return (
+      <div className={divClass} style={divStyle}> 
+        <label htmlFor={name}>{label}</label>
+        <input name={name} type={type} id={name} onChange={handleChange} placeholder={placeHolder} style={inputStyle}/>
+      </div>
+  )
+  }
+
+  const  renderDiv = () => {
+    const components = [];
+    
+    for(let i = 0;i<numberOfDivs;i++){
+      components.push(<CompleteComponent key={`child-${i}`}/>);
+      console.log('created button number:' + `${i}`);
+    }
+    return components;
+  };
+
+
   return (
-    <div className={divClass} style={divStyle}> 
-      <label htmlFor={name}>{label}</label>
-      <input name={name} type={type} id={name} onChange={handleChange} placeholder={placeHolder} style={inputStyle}/>
-      
-    </div>
-)}
+    <>
+      {renderDiv()}
+      <button onClick={incrementDivs}>+</button>
+    </>
+  )
+  
+}
   
