981ac2a2dcffc91a4b597c2bc4e3f3686a663949
iversson-estudos <i versson.estudos@gmail.com>
2025-05-02 14:29:55 -0300

    Done, component now receives a value that defines how many divs will be created, also has a button that if pressed creates one more div, and so on

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -14,7 +14,7 @@ export  function Form() {
                   <TextInputLabeled divClass={styles.name} name='name' label='Name' placeHolder='Enter your name' type='text' size={24} />
                 </div>
                 <div className={styles.experienceContainer}>
-                    <MultiDivTextArea divClass={styles.experience} name='experience' count={1} placeHolder='Past Experiences' label='Experience' size={80} />
+                    <MultiDivTextArea divClass={styles.experience} name='experience'  placeHolder='Past Experiences' label='Experience' size={80} />
                     <MultiDivTextArea divClass={styles.experience} name='company' placeHolder='Company' label='Company' size={24} />
                 </div>
               </form>
--- src/components/Inputs/MultiDivTextArea.jsx
+++ src/components/Inputs/MultiDivTextArea.jsx
@@ -1,6 +1,6 @@
 import {React,useState} from "react";
 
-export function MultiDivTextArea({name,count=1,type,size,label,divClass,handleChange,placeHolder='...'}) {
+export function MultiDivTextArea({name,count=1,type,size,label,divClass,placeHolder='...'}) {
 /*STYLES*/  
   const divStyle = {
     display:'flex', 
@@ -10,41 +10,39 @@ export function MultiDivTextArea({name,count=1,type,size,label,divClass,handleCh
   height:`${size}px` 
   }
    
-   /*USE STATE FOR DIV COUNTER*/
-   const [divCount,setDivCount]= useState(count);
+
    /*USE STATE FOR DIV VALUES, EMPTY ARRAY SIZE OF DIV COUNT*/
    const [values,setValues]= useState(Array(count).fill(''));
 
 
   /*HANDLE TEXT CHANGES*/ 
-  const handleTextChanges = (index,value)=>{
+  const handleTextChange = (index,value)=>{
     const newValues = [...values];
     newValues[index]=value;
     setValues(newValues);
   }
 
   /*CLICK HANDLER*/
-  const handleClick=()=>{
-    setDivCount = (prevCount=>prevCount+1);
-    setValues(prevValues=>[...prevValues,'']);
-  }
+  const handleClick = () => {
+    setValues(prevValues => [...prevValues, '']); 
+  };
 
 
 
 
    /*CREATES DIVS*/
-   const Component = [];
-
-   for(let i = 0 ; i<divCount;i++){
-    Component.push(
+   const Component = values.map((value,i)=>(
     <div style={divStyle} className={divClass} key={`div-${i}`}>
-      <label htmlFor={`${name}-${i}`}>{label}</label>
-      <textarea name={`${name}-${i}`} key={`textarea-${i}`} type={type} id={`${name}-${i}`} placeholder={placeHolder} onChange={handleChange} style={inputStyle}/>
+      <label htmlFor={`${name}-${i}`}>{label} {i+1}</label>
+      <textarea name={`${name}-${i}`}  type={type} id={`${name}-${i}`} placeholder={placeHolder} onChange={(e)=>handleTextChange(i,e.target.value)} value={value} style={inputStyle}/>
     </div>
-    );
+   ));
+
+   
+   
 
 
-   }
+   
    
 
 
@@ -52,6 +50,7 @@ export function MultiDivTextArea({name,count=1,type,size,label,divClass,handleCh
   return (
     <>
       {Component}
+      <button type="button" onClick={handleClick}>+</button>
     </>
 )}
   
